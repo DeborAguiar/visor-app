4d351605312c9b7afe3f53256a37f04a154a4736
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-12-28 14:59:53 -0300

    initial ajustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LanguageComponent } from './components/language/language.component';
+import { CodeComponent } from './components/code/code.component';
+import { PlaceComponent } from './components/place/place.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LanguageComponent },
+  { path: ':language', children: [
+    { path: 'code', component: CodeComponent },
+    { path: 'place/:id', component: PlaceComponent},
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { CodeComponent } from './components/code/code.component';
+import { LanguageComponent } from './components/language/language.component';
+import { PlaceComponent } from './components/place/place.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeComponent,
+    LanguageComponent,
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [
     provideClientHydration()
