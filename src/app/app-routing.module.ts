d97e77b5ee41a1b0fd610d3146f93f02a7b8c7b7
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-12-28 23:13:37 -0300

    change routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LanguageComponent } from './components/language/language.component';
 import { CodeComponent } from './components/code/code.component';
+import { LanguageComponent } from './components/language/language.component';
 import { PlaceComponent } from './components/place/place.component';
+import { ItemComponent } from './components/item/item.component';
 
 const routes: Routes = [
   { path: '', component: LanguageComponent },
-  { path: ':language', children: [
-    { path: 'code', component: CodeComponent },
-    { path: 'place/:id', component: PlaceComponent},
-  ] },
+  { path: ':language/code', component: CodeComponent, pathMatch: 'full' },
+  { path: ':language/place/:idPlace', component: PlaceComponent, pathMatch: 'full'  },
+  { path: ':language/place/:idPlace/item/:idItem', component: ItemComponent, pathMatch: 'full'  },
 ];
 
 @NgModule({
