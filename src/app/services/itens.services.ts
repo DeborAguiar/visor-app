5b34d222000efbab9268a1fc5a80642044624029
DeborAguiar <50847540+DeborAguiar@users.noreply.github.com>
2023-12-29 01:00:12 -0300

    update item.service

--- src/app/services/itens.services.ts
+++ src/app/services/itens.services.ts
@@ -9,6 +9,10 @@ export class ItensService {
     getAllItensByPlaceId(placeId: number): { id: number, nome: string }[] {
         return itens
             .filter(item => item.id_museu == placeId)
-            .map((item) => { return { id: item.id_obra, nome: item.nome_obra } })
+            .map((item) => { return { id: item.id_obra, nome: item.name } })
+    }
+
+    getItemById(itemId: number): Item | undefined {
+        return itens.find(item => item.id_obra == itemId) as Item;
     }
 }
